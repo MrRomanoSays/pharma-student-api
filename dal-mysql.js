a3702b2a2e50a2fba2b1a720d9a4aa97daf3c6e8
Lawrence Romano <l.j.romano@gmail.com>
2017-03-17 16:59:32 -0400

    mysql getPatient, getPatients, getMed, listMedsbyLabel and getPharmacy working.  Pagination on getPatients and listMedsByLabel.  Noticed that pagination on mysql affected nosql.  Will need to address adding startkey and limit functionality inputs in app.js for couch

--- dal-mysql.js
+++ dal-mysql.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const moment = require('moment')
 
 const {
     map,
@@ -13,12 +14,18 @@ const {
     view,
     lensIndex,
     set,
-    lensPath
+    lensPath,
+    toString,
+    lensProp
 } = require('ramda')
 
 
 var dal = {
-    getMed: getMed
+    getPatient: getPatient,
+    getPatients: getPatients,
+    getMed: getMed,
+    listMedsByLabel: listMedsByLabel,
+    getPharmacy: getPharmacy
 }
 
 module.exports = dal
@@ -34,6 +41,276 @@ function getMed(id, callback) {
     })
 }
 
+
+function getMed(medId, cb) {
+    // getDocByID('medWithIngredients', id, formatMed, function(err, res) {
+    //     if (err) return callback(err)
+    //     //callback(null, view(lensIndex(0), res))
+    //     callback(null, res)
+    // })
+    if (!medId) return cb({
+        error: 'missing_id',
+        reason: 'missing_id',
+        name: 'missing_id',
+        status: 400,
+        message: 'unable to retrieve medication due to missing id.'
+    })
+
+    const connection = createConnection()
+
+    connection.query('SELECT * FROM medWithIngredients WHERE ID = ?', [medId], function(err, medRows) {
+        if (err) return cb({
+            error: 'unknown',
+            reason: 'unknown',
+            name: 'unknown',
+            status: 500,
+            message: err.message
+        })
+        if (medRows.length === 0) return cb({
+            error: 'not_found',
+            reason: 'missing',
+            name: 'not_found',
+            status: 404,
+            message: 'You have attempted to retrieve a medication that is not in the database or has been deleted.'
+        })
+
+        cb(null, formatSingleMed(medRows))
+    })
+}
+
+function formatMultipleMeds(meds) {
+
+    const uniqueIds = compose (
+      uniq(),
+      map(med=>med.ID))(meds)
+
+    return map(id=>compose(
+        formatSingleMed,
+        filter(med=>med.ID === id)
+    )(meds))(uniqueIds)
+}
+
+//////////////////////////////////////////////////////
+/////// HELPER FUNCTIONS FOR FORMATTING MEDS
+//////////////////////////////////////////////////////
+
+function formatSingleMed(medRows) {
+
+  const mappedIngredients = compose(
+      map(med => med.ingredient),
+      filter(med => med.ingredient)
+  )(medRows)
+
+  return compose(
+    omit(['ID', 'ingredient']),
+    set(lensProp('ingredients'), mappedIngredients),
+    set(lensProp('_id'), toString(prop('ID', medRows[0]))),
+    set(lensProp('_rev'), ""),
+    set(lensProp('type'), "medication")
+  )(medRows[0])
+
+}
+
+
+function listMedsByLabel (startKey, limit, cb) {
+  const connection = createConnection()
+  //PAGINATION, default limit of 5...
+  limit = limit ? limit : 5
+  const whereClause = startKey ? " WHERE concat(m.label, m.ID) > '" + startKey + "'" : ""
+
+  let sql = 'SELECT m.*, concat(m.label, m.ID) as startKey '
+  sql += ' FROM medWithIngredients m '
+  sql += ' INNER JOIN (SELECT DISTINCT ID '
+  sql += ' FROM medWithIngredients m'
+  sql += whereClause
+  sql += ' LIMIT ' + limit + ') b '
+  sql += ' ON m.ID = b.ID '
+  sql += whereClause
+  sql += ' ORDER BY startKey'
+
+  // console.log("sql: ", sql)
+
+  connection.query(sql, function(err, data) {
+    if(err) return cb({
+      error: 'unknown',
+      reason: 'unknown',
+      name: 'unknown',
+      status: 500,
+      message: err.message
+    })
+    if (data.length === 0) return cb({
+      error: 'not_found',
+      reason: 'missing',
+      name: 'not_found',
+      status: '404',
+      message: 'You have attempted to retreive medications that are not in the database.'
+    })
+    cb(null, formatMultipleMeds(data))
+  })
+}
+
+
+/////////////////////
+// PATIENTS
+/////////////////////
+
+function getPatient(patientId, cb) {
+
+  if (!patientId) return cb({
+      error: 'missing_id',
+      reason: 'missing_id',
+      name: 'missing_id',
+      status: 400,
+      message: 'unable to retrieve data due to missing id.'
+  })
+
+  const connection = createConnection()
+
+  connection.query('SELECT * FROM patientWithConditions WHERE ID = ?', [patientId], function(err, data) {
+      if (err) return cb({
+          error: 'unknown',
+          reason: 'unknown',
+          name: 'unknown',
+          status: 500,
+          message: err.message
+      })
+      if (data.length === 0) return cb({
+          error: 'not_found',
+          reason: 'missing',
+          name: 'not_found',
+          status: 404,
+          message: 'missing'
+      })
+
+
+      cb(null, formatSinglePatient(data))
+  })
+}
+
+function getPatients(startKey, limit, cb) {
+
+  const connection = createConnection()
+
+  //PAGINATION FOR PATIENTS
+  limit = limit ? limit : 5
+  const whereClause = startKey ? " WHERE concat(p.lastName, p.ID) > '" + startKey + "'" : ""
+
+  let sql = 'SELECT p.*, concat(p.lastName, p.ID) as startKey '
+  sql += ' FROM patientWithConditions p '
+  sql += ' INNER JOIN (SELECT DISTINCT ID '
+  sql += ' FROM patientWithConditions p'
+  sql += whereClause
+  sql += ' LIMIT ' + limit + ') b '
+  sql += ' ON p.ID = b.ID '
+  sql += whereClause
+  sql += ' ORDER BY startKey'
+
+  console.log("pagination sql for patients: ", sql)
+
+
+  //let sql = 'SELECT * FROM pharmaStudent.patientWithConditions;'
+
+  connection.query(sql, function(err, data) {
+  if (err) return cb(errorMessage)
+
+  if (data.length === 0) return cb(noDataFound)
+
+      cb(null, formatMultiplePatients(data))
+})
+
+}
+
+/////////////////////
+// PATIENT HELPER FUNCTIONS
+/////////////////////
+function formatSinglePatient(patientRows) {
+
+  const mappedConditions = compose(
+    map(patient => patient.condition),
+    filter(patient => patient.condition)
+  )(patientRows)
+
+  return  compose(
+    set(lensProp('birthdate'), moment(prop('birthdate', patientRows[0])).format("YYYY-MM-DD")),
+    set(lensProp('conditions') , mappedConditions),
+    omit(['ID', 'condition']),
+    set(lensProp('_id'), toString(prop('ID', patientRows[0]))),
+    set(lensProp('_rev'), ""),
+    set(lensProp('type'), "patient")
+  )(patientRows[0])
+}
+
+
+function formatMultiplePatients(patients) {
+
+  const IDs = compose(
+  uniq(),
+  map(patient => patient.ID)
+)(patients)
+
+ return map(id => compose(
+formatSinglePatient,
+filter(patient => patient.ID === id)
+)(patients))(IDs)
+
+}
+
+
+/////////////////////
+// PHARMACIES
+/////////////////////
+
+function getPharmacy(pharmacyId, cb) {
+
+  if(!pharmacyId) return cb({
+    error: 'missing_id',
+    reason: 'missing_id',
+    name: 'missing_id',
+    status: 400,
+    message: 'unable to retrieve data due to missing id.'
+  })
+
+  const connection = createConnection()
+
+  connection.query('SELECT * FROM pharmacy WHERE ID = ?', [pharmacyId], function (err, data) {
+    if (err) return cb({
+        error: 'unknown',
+        reason: 'unknown',
+        name: 'unknown',
+        status: 500,
+        message: err.message
+    })
+    if (data.length === 0) return cb({
+        error: 'not_found',
+        reason: 'missing',
+        name: 'not_found',
+        status: 404,
+        message: 'missing'
+    })
+
+    const typeLens = lensProp('type');
+    const revLens = lensProp('_rev');
+    const idLens = lensProp('_id')
+
+    set(revLens, 'pharmacy', data[0]);
+    set(typeLens, 'pharmacy', data[0]);
+
+    let idValue = prop('ID', data[0])
+    idValue = toString(idValue)
+
+    const theResult = compose(
+      omit('ID'),
+      set(idLens, idValue),
+      set(revLens, ''),
+      set(typeLens, 'pharmacy')
+    )(data[0])
+
+    cb(null, theResult)
+
+  })
+}
+
+
 /////////////////////
 // helper functions
 /////////////////////
@@ -42,8 +319,8 @@ function createConnection() {
     return mysql.createConnection({
         host: "0.0.0.0",
         user: "root",
-        password: "mypassword",
-        database: "test-pharma-student"
+        password: "viewsonic",
+        database: "pharmaStudent"
     });
 }
 
@@ -141,3 +418,38 @@ const convertNoSQLFormat = row => compose(
       omit(['ID']),
       set(lensPath(['_id']), path(['ID'], row))
     )(row)
+
+
+////////////////////////////////
+// ERROR RESPONSE HELPERS
+////////////////////////////////
+
+function notFound() {
+  return {
+      error: 'missing_id',
+      reason: 'missing_id',
+      name: 'missing_id',
+      status: 400,
+      message: 'Unable to retrieve data due to missing id.'
+  }
+}
+
+function errorMessage() {
+  return {
+      error: 'unknown',
+      reason: 'unknown',
+      name: 'unknown',
+      status: 500,
+      message: err.message
+  }
+}
+
+function noDataFound() {
+  return {
+      error: 'not_found',
+      reason: 'missing',
+      name: 'not_found',
+      status: 404,
+      message: 'missing'
+  }
+}
